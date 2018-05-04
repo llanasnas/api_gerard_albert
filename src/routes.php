b8adc9e8f4e0cec59ed1bdf5548067483c04e541
AlexCods <alexalji@gmail.com>
2018-05-04 19:53:08 +0200

    commit and push

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,19 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gelatina";
+
+$GLOBALS['conn'] = new mysqli($servername, $username, $password, $dbname);
+$GLOBALS['datos'] = array();
+
+if ($GLOBALS['conn']->connect_error) {
+    die("Connection failed: " . $GLOBALS['conn']->connect_error);
+}
+
+
 // Routes
 $app->get('/',function (Request $request, Response $response, array $args){
     $args["code"] = "200";
@@ -11,9 +24,123 @@ $app->get('/',function (Request $request, Response $response, array $args){
     return $response;
 });
 // Routes
-$app->get('/',function (Request $request, Response $response, array $args){
+$app->get('/getAll',function (Request $request, Response $response, array $args){
+
+    $getAll = "SELECT * FROM notes";
+    $result = $GLOBALS['conn']->query($getAll);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($GLOBALS['datos'],$row);
+        }
+        $args["code"] = "200";
+        $args["resp"] = $GLOBALS['datos'];
+
+    }else{
+        $args["code"] = "204";
+        $args["msg"] = "Notes not found!";
+    }
+    $response= $response->withJson($args,200);
 
     return $response;
 });
+
+
+$app->get('/getPublic',function (Request $request, Response $response, array $args){
+
+    $getAll = "SELECT * FROM notes WHERE private <> 1";
+    $result = $GLOBALS['conn']->query($getAll);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($GLOBALS['datos'],$row);
+        }
+        $args["code"] = "200";
+        $args["resp"] = $GLOBALS['datos'];
+
+    }else{
+        $args["code"] = "204";
+        $args["msg"] = "Notes not found!";
+    }
+    $response= $response->withJson($args,200);
+
+    return $response;
+});
+
+$app->get('/getOne/{id}',function (Request $request, Response $response, array $args){
+
+    $getAll = "SELECT * FROM notes WHERE id = " . $args['id'];
+    $result = $GLOBALS['conn']->query($getAll);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($GLOBALS['datos'],$row);
+        }
+        $args["code"] = "200";
+        $args["resp"] = $GLOBALS['datos'];
+        $response= $response->withJson($args,200);
+    }else{
+        $args["code"] = "204";
+        $args["msg"] = "Notes not found!";
+    }
+
+    return $response;
+});
+
+$app->get('/insert/{title}/{content}/{private}/{tag1}/{tag2}/{tag3}/{tag4}/{book}/{createData}/{lastModification}/{user}',function (Request $request, Response $response, array $args){
+    $query = "INSERT INTO notes VALUES (null,?,?,?,?,?,?,?,?,NOW(),NOW(),?)";
+    $insertGelatina = $GLOBALS['conn']->prepare($query);
+    $insertGelatina->bind_param("sssssssss", $args['title'], $args['content'], $args['private'], $args['tag1'], $args['tag2'], $args['tag3'], $args['tag4'],$args['book'], $args['user']);
+
+
+
+    if ($insertGelatina->execute()) {
+        $args["code"] = "200";
+        $args["msg"] = "Note inserted!";
+        $response= $response->withJson($args,200);
+    }
+
+    return $response;
+});
+
+$app->get('/remove/{id}',function (Request $request, Response $response, array $args){
+
+    $query = "DELETE FROM notes WHERE id = '".$args['id']."'";
+
+    if($GLOBALS["conn"]->query($query)){
+        $args["code"] = "200";
+        $args["msg"] = "note deleted!";
+        $response= $response->withJson($args,200);
+    }
+    return $response;
+});
+
+$app->get('/getAllWithTag/{tag}',function (Request $request, Response $response, array $args){
+
+    $query = "SELECT * FROM notes WHERE tag1 LIKE ? OR tag2 LIKE ? OR tag3 LIKE ? OR tag4 LIKE ?";
+    $stmt = $GLOBALS['conn']->prepare($query);
+    $aux = "%" . $args['tag'] . "%";
+    $stmt->bind_param("ssss",$aux,$aux,$aux,$aux);
+    $result = $stmt -> execute();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($GLOBALS['datos'],$row);
+            }
+            $args["code"] = "200";
+            $args["resp"] = $GLOBALS['datos'];
+
+        }else{
+            $args["code"] = "204";
+            $args["msg"] = "Notes not found!";
+        }
+    $response= $response->withJson($args,200);
+
+    return $response;
+
+
+});
+?>
+
 
 
